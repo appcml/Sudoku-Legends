1758ed5ebc7c9d736fc216c44ce1183ae96e4d86
appcml <35844750+appcml@users.noreply.github.com>
2026-07-14 16:26:15 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Sudoku-Legends/',
+  base: './',
   server: {
     port: 3000,
     host: true,
